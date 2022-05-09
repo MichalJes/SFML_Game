9e99d9ea79331e5db9324eca24a92611ec44ff0f
Kala <63319112+azukowa@users.noreply.github.com>
2022-05-09 18:40:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+
+class CustomRectangleShape : public sf::RectangleShape {
+public:
+    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f &position,
+                         const sf::RenderWindow &window)
+        : sf::RectangleShape(size), window_(window) {
+        setPosition(position);
+    };
+
 void setSpeed(int x, int y, int rotation) {
         speed_x_ = x;
         speed_y_ = y;
@@ -45,23 +55,52 @@ void setBounds(const sf::IntRect &bounds) {
 void setBounds(int left, int width, int top, int height) {
         bounds_ = sf::IntRect(left, top, width, height);
 }
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    sf::Clock clock;
-    while (window.isOpen())
-    {
+
+private:
+    const sf::RenderWindow &window_;
+    int speed_x_ = 0;
+    int speed_y_ = 0;
+    int speed_rotation_ = 0;
+    sf::IntRect bounds_;
+};
+
+int main() {
+    // stworzenie okna
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    // tworzymy prostokąt wg kreatora z klasy
+    CustomRectangleShape rectangle(sf::Vector2f(120.0, 60.0),
+                                   sf::Vector2f(300.0, 300.0),
+                                   window);
+    rectangle.setFillColor(sf::Color(100, 50, 250));
+    //ustawienie prędskosci
+    rectangle.setSpeed(100, 100, 10);
+
+    rectangle.setBounds(0, window.getSize().x, 0, window.getSize().y);
+
+    sf::Clock clock;//zegar przed petla
+
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window and kohać dziekana
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        sf::Time elapsed = clock.restart();
+
+        rectangle.animate(elapsed);
+
+        // draw everything here...
+        window.draw(rectangle);
+
+        // end the current frame
         window.display();
     }
 
