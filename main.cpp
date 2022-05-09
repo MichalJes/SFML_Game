018483f680e766faa878a47f1199e5c7d24dbbc3
Michał Jeske <michal.jes@gmail.com>
2022-05-09 15:31:46 +0200

    Gamestate&Spaceclicks

--- main.cpp
+++ main.cpp
@@ -1,23 +1,65 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <string>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+int main() {
+    // Window setup
+    sf::RenderWindow mainWindow(sf::VideoMode(640, 480), "This is main window", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event ev;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+    int spaceClicks = -1;
+    int gameState = 0;
+    int col = 100;
+
+    // App loop
+    while (mainWindow.isOpen()) {
+        // Event capture
+        while (mainWindow.pollEvent(ev)) {
+            switch (ev.type) {
+                case sf::Event::Closed:
+                    mainWindow.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (ev.key.code == sf::Keyboard::Escape) {
+                        mainWindow.close();
+                        break;
+                    }
+                    if (ev.key.code == sf::Keyboard::Space) {
+                        if (spaceClicks == -1) {
+                            gameState = 1;
+                            spaceClicks = +1;
+                            break;
+                        } else {
+                            gameState = 2;
+                            if (col + 10 > 255) {
+                                col = 90;
+                            }
+                            col += 10;
+                            break;
+                        }
+                    }
+            }
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        // Update
+
+        // Render
+        switch (gameState) {
+            case 0:
+                mainWindow.clear(sf::Color::Blue);
+                break;
+            case 1:
+                mainWindow.clear(sf::Color::Green);
+                break;
+            default:
+                mainWindow.clear(sf::Color(255, 255, col, 255));
+                break;
+        }
+
+        mainWindow.display();
     }
 
     return 0;
